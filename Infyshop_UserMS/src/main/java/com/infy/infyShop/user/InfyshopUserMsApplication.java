282f4c7ec22f1e2c3c0096187e67a77ecd2f00ce
mohit <mohit@DESKTOP-R8FRI8Q>
2021-05-20 00:15:10 +0530

    commit

--- Infyshop_UserMS/src/main/java/com/infy/infyShop/user/InfyshopUserMsApplication.java
+++ Infyshop_UserMS/src/main/java/com/infy/infyShop/user/InfyshopUserMsApplication.java
@@ -2,8 +2,10 @@ package com.infy.infyShop.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InfyshopUserMsApplication {
 
 	public static void main(String[] args) {
